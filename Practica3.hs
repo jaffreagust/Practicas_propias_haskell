f3cd1a791726e1363edbf4d766e2df582e2c9676
jaffreagust <jaffreagustin18205@gmail.com>
2023-05-23 12:37:53 -0300

    Ejercicio 9 resuelto

--- Practica3.hs
+++ Practica3.hs
@@ -123,6 +123,43 @@ checkLHeap (Nod _ x Em  r@(Nod _ y _ _)) = (x<y) && checkLHeap r
 checkLHeap (Nod _ x l@(Nod r1 y _ _) r@(Nod r2 z _ _)) = (x < y) && (x < z) && checkLHeap l && checkLHeap r && r1 >= r2
 
 --f)--
+--Fusion entre dos Leftist Heap--
+merge :: Ord a => Heap a -> Heap a -> Heap a
+merge h1 Em = h1
+merge Em h2 = h2
+merge h1@(Nod _ x a1 b1) h2@(Nod _ y a2 b2) =
+    if x <= y then makeH x a1 (merge b1 h2)
+              else makeH y a2 (merge h1 b2)
+
+--Extrae el rango
+rank :: Heap a -> Rank
+rank Em = 0
+rank (Nod r _ _ _) = r
+
+--Consulta el rango de los dos nodos
+-- Si es mas grande el rango de b se incrementa en uno por la raiz añadida
+-- el mas grande pasara a ser la espina izquierda (se menciona primero), y la menor será la derecha
+makeH x a b = if rank a >= rank b then Nod (rank b + 1) x a b
+                                  else Nod (rank a + 1) x b a
+
+fromList [] = Em
+fromList xs = let hs = map (\x -> Nod 1 x Em Em) xs
+                  pares [] = []
+                  pares [x] = [x]
+                  pares (x:y:ys) = merge x y : pares ys
+                  g [] = Em
+                  g [h] = h
+                  g ys = g (pares ys)
+                  in g hs
+
+onlyonex x [] = []
+onlyonex x (y:ys) = if (x==y) then onlyonex x ys
+                              else y:onlyonex x ys
+
+unique [] = []
+unique (x:xs) = x:(onlyonex x (unique xs))
+
+notdupLHeap heap = fromList (unique (inorden heap)) 
 
 --g)--
 data Color = Re | B deriving Show
