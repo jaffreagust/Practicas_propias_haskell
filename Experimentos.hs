542c7ebd1a6f8f94f696dbc1af20534f69ff68e3
caffre <jaffreagustin18205@gmail.com>
2023-06-07 11:09:15 -0300

    delete BST

--- Experimentos.hs
+++ Experimentos.hs
@@ -264,4 +264,17 @@ balancedBST x n | even(n-1) = let m = div (n-1) 2
                                   (t1,t2) = balancedBST' x m 
                                   in N t1 x t2
                                         where balancedBST' x m =(balancedBST x (m+1),balancedBST x m)
-                                   
+
+minimun (H) = 0
+minimun (N H x r) = x 
+minimun (N l x r) = minimun l
+
+deleteBTS _ H = H
+deleteBTS x (N H y H) | x == y = H 
+deleteBTS x (N H y r) | x == y = r 
+deleteBTS x (N l y H) | x == y = l   
+deleteBTS x (N l y r) | x< y = N (deleteBTS x l) y r 
+                      | x> y = N l y (deleteBTS x r)  
+                      | x == y = let b =minimun r
+                                     in N l b (deleteBTS x r)
+        
